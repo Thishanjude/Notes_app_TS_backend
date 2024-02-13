79a1aed64d93fb374f0947136ba0ac677b07cbd1
ThishanJude <thishan1997@gmail.com>
2024-02-13 23:31:33 +0530

    added new routes and controllers for project structure, created new APIs and added error handling

---
+++ src/routes/notes.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import * as NotesController from "../controllers/notes";
+
+const router = express.Router();
+
+router.get("/", NotesController.getNotes);
+router.get("/:noteId", NotesController.getNote)
+
+router.post("/", NotesController.createNotes);
+
+export default router;
