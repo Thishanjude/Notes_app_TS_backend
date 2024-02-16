96d2d82fb44f34fc8e1274090c42471b98ca38db
ThishanJude <thishan1997@gmail.com>
2024-02-16 15:47:11 +0530

    added user authentication with sessions

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,6 +3,10 @@ import * as userController from "../controllers/users";
 
 const router = express.Router();
 
+router.get("/", userController.getAuthenticatedUser);
+
 router.post("/signup", userController.signUp);
+router.post("/login", userController.login);
+router.post("/logout", userController.logout);
 
 export default router;
